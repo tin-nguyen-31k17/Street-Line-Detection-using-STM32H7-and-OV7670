6757419450a994c3f687de082d62f43ad34ca3ed
Fusioz <trongtin.0301@gmail.com>
2022-12-28 23:47:21 +0700

    add src and main.c

---
+++ src/main.c
@@ -0,0 +1,117 @@
+#include "stm32f1xx_hal.h"
+#include "ov7670.h"  // include header file for OV7670 camera module
+
+// define pins for communication with OV7670 camera module
+#define CAMERA_SIOC_PORT  GPIOC
+#define CAMERA_SIOC_PIN   GPIO_PIN_6
+#define CAMERA_SIOD_PORT  GPIOC
+#define CAMERA_SIOD_PIN   GPIO_PIN_7
+
+// define clock and power pins for OV7670 camera module
+#define CAMERA_CLK_PORT   GPIOC
+#define CAMERA_CLK_PIN    GPIO_PIN_8
+#define CAMERA_PWR_PORT   GPIOC
+#define CAMERA_PWR_PIN    GPIO_PIN_9
+
+#define CAMERA_BUFFER_SIZE  1024  // size of camera image buffer
+uint8_t camera_buffer[CAMERA_BUFFER_SIZE];  // camera image buffer
+
+    // function to start capturing images with OV7670 camera module
+void camera_start_capture(void)
+{
+    // set image format and resolution in COM7 register
+    ov7670_write_reg(OV7670_COM7, OV7670_COM7_FMT_RGB565 | OV7670_COM7_RES_QVGA);
+
+    // set image capture mode in COM2 register
+    ov7670_write_reg(OV7670_COM2, OV7670_COM2_SSLEEP);
+}
+
+// function to process captured image and detect street lines
+void detect_street_lines(uint8_t *image, int width, int height)
+{
+    // apply image processing techniques such as edge detection or line detection
+    // to the captured image to detect the street lines
+
+    // store detected street lines in a data structure or global variable
+}
+
+int main(void)
+{
+    // initialize OV7670 camera module
+    camera_init();
+    
+    // start capturing images with OV7670 camera module
+    camera_start_capture();
+
+    while (1)
+    {
+        // continuously capture images from OV7670 camera module
+        if (HAL_I2C_Master_Receive(&hi2c1, OV7670_I2C_ADDR, camera_buffer, CAMERA_BUFFER_SIZE, 100))
+    }
+    //
+}
+// function to initialize OV7670 camera module
+void camera_init(void)
+{
+    // set image size and output format in COM7 register
+    ov7670_write_reg(OV7670_COM7, OV7670_COM7_QVGA | OV7670_COM7_RGB);
+
+    // set frame rate in COM3 register
+    ov7670_write_reg(OV7670_COM3, OV7670_COM3_DCWEN);
+
+    // set output data format in COM14 register
+    ov7670_write_reg(OV7670_COM14, OV7670_COM14_DCWEN | OV7670_COM14_MANU);
+}
+// function to process captured image and detect street lines
+void detect_street_lines(uint8_t *image, int width, int height)
+{
+    // apply image processing techniques such as edge detection or line detection
+    // to the captured image to detect the street lines
+
+    // store detected street lines in global array
+    num_street_lines = 0;  // reset number of detected street lines
+    for (int i = 0; i < 10; i++)
+    {
+        if (detect_line(image, width, height, &street_lines[i]))
+        {
+            num_street_lines++;  // increment number of detected street lines
+        }
+    }
+
+    // analyze detected street lines and determine appropriate action for robotic vehicle
+    if (num_street_lines == 0)
+    {
+        // no street lines detected, stop the robotic vehicle
+        stop_vehicle();
+    }
+    else if (num_street_lines == 1)
+    {
+        // one street line detected, check its position and adjust speed accordingly
+        if (street_lines[0].x1 < width / 2)
+        {
+            // street line is on the left side of the image, turn right and slow down
+            turn_right();
+            slow_down();
+        }
+        else
+        {
+            // street line is on the right side of the image, turn left and slow down
+            turn_left();
+            slow_down();
+        }
+    }
+    else
+    {
+        // two or more street lines detected, check their positions and adjust speed accordingly
+        if (street_lines[0].x1 < width / 2 && street_lines[1].x1 > width / 2)
+        {
+            // street lines are on opposite sides of the image, proceed straight and maintain current speed
+            proceed_straight();
+        }
+        else
+        {
+            // street lines are on the same side of the image, slow down
+            slow_down();
+        }
+    }
+}
